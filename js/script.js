a83a90d91c2afb16251eef2b1f833e8148098812
Farabi islam Nurullah <farabi48islam@gmail.com>
2023-07-18 01:31:45 +0600

    mid

--- js/script.js
+++ js/script.js
@@ -33,3 +33,8 @@ $(document).ready(function(){
         }
     })
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
