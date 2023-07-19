d4873a955e318c0468d3bfc51f93149ec7fb097e
Farabi islam Nurullah <farabi48islam@gmail.com>
2023-07-20 02:40:24 +0600

    final updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+    $('#logo-slider').owlCarousel({
+        loop:true,
+        margin:45,
+        nav:true,
+        dots: false, 
+        responsiveClass:true,
+        navText : ["<i class='fa-light fa-chevron-left'></i>","<i class='fa-light fa-chevron-right'></i>"],
+        responsive:{
+            0:{
+                nav:false,
+                margin:25,
+                items:3,  
+            },
+            576:{
+                nav:true,
+            },
+            992:{    
+                items:4,  
+                margin:35,
+            },   
+            1200:{   
+            },
+            1600:{
+                margin:25,
+                items:5,               
+            }
+        }
+    })
+});
+$(document).ready(function(){
     $('#multi-slider').owlCarousel({
         loop:true,
         margin:60,
@@ -34,7 +64,10 @@ $(document).ready(function(){
     })
 });
 
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
